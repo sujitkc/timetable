6b70439cd4c8fe0187f9886277cb001fc8c46300
sujitkc <sujitkc@gmail.com>
2019-01-22 14:53:16 +0530

    schedule2

--- software/schedule.py
+++ software/schedule.py
@@ -5,6 +5,7 @@ light = (2, 3)
 vheavy = (3, 3)
 vlight = (2, 2)
 
+periodsPerDay = 8
 loads = (heavy, light, vheavy, vlight)
 
 participants = []
@@ -20,20 +21,23 @@ def setup():
 	global slots
 	global subjectLoads
 	global participantSubjects
+	global periodsPerDay
 
-	participants = [x + 1 for x in range(50)]
-	subjects = [x + 1 for x in range(20)]
-	slots = [x + 1 for x in range(60)]
+	participants = range(50)
+	slots = []
+	for day in range(5):
+		for period in range(periodsPerDay):
+			slots.append((day, period)) 
 
-	for x in range(len(subjects)):
-		subjectLoads[participants[x]] = loads[x % len(loads)]
+	for x in range(20):
+		subjects.append((x, loads[x % len(loads)])) # subject = (id, loadType)
 
 	for p in participants:
 		s = []
 		for i in range(2):
 			s1 = subjects[random.randint(0, len(subjects)) - 1]
 			s.append(s1)
-		participantSubjects[p] = set(s)
+		participantSubjects[p] = set(s) # participantSubjects = {participant_1 : set[subject1, subject2, ...], participant_2 : set[subject1, subject2, ...],}
 
 graph = {}
 def makeGraph():
@@ -56,7 +60,7 @@ def makeGraph():
 
 def areNeighbours(n1, n2):
 	global graph
-	return n1 in graph[n2]
+	return n1 in graph[n2] # n1 in graph[n2] <=> n2 in graph[n1] by construction
 
 def schedule():
 	global graph
@@ -66,48 +70,71 @@ def schedule():
 	global subjectLoads
 	global participantSubjects
 
-	goodChoices = {}
-	goodChoices[heavy] = ((0, 2), (1, 3), (3, 5))
-	goodChoices[light] = ((1, 3, 5), (1, 2, 3), (2, 3, 4), (3, 4, 5))
-	goodChoices[vheavy] = ((1, 3, 5), (1, 2, 3), (2, 3, 4), (3, 4, 5))
-	goodChoices[vlight] = ((0, 2), (1, 3), (3, 5))
+
+# internal functions - begin
 
 	theSchedule = {x : [] for x in slots}
 	def isSlotAvailable(subject, groupSize, slot):
-		if(slot > (len(slots) - groupSize)):
+		day = slot[0]
+		period = slot[1]
+		if(period > (periodsPerDay - groupSize)):
 			return False
 
 		for x in range(groupSize):
-			s2list = theSchedule[slot + x]
+			s2list = theSchedule[(day, period + x)]
 			for s2 in s2list:
 				if(areNeighbours(subject, s2)):
 					return False
 		return True
 
+	def isSlotGroupAvailable(subject, slotGroup):
+		groupSize = subject[1][1]
+		for slot in slotGroup:
+			if(not isSlotAvailable(subject, groupSize, slot)):
+				return False
+		return True
 
 
-	flat = []
-	for s in subjectLoads:
-		load = subjectLoads[s]
-		groupSize = load[0]
-		lectures = load[1]
-		entry = [(s, groupSize)] * lectures
-		flat.extend(entry)	
+	def assignSubjectToSlot(subject, slot):
+		groupSize = subject[1][1]
+		for i in range(groupSize):
+			theSchedule[(slot[0], slot[1] + i)].append(subject)
 
-#	print 'flat = ' + str(flat)
-	for entry in flat:
-		s = entry[0]
-		groupSize = entry[1]
-		for slot in slots:
-			done = False
-			if(isSlotAvailable(s, groupSize, slot)):
-				for x in range(groupSize):
-					theSchedule[slot + x].append(s)
-				done = True
-			if(done == True):
+# internal functions - end
+	goodChoices = {}
+	goodChoices[heavy] = ((0, 2), (1, 3), (2, 4))
+	goodChoices[light] = ((0, 2, 4), (1, 2, 3), (0, 1, 2), (2, 3, 4))
+	goodChoices[vheavy] = ((0, 2, 4), (1, 2, 3), (0, 1, 2), (2, 3, 4))
+	goodChoices[vlight] = ((0, 2), (1, 3), (2, 4))
+
+
+	for subject in subjects:
+		loadType = subject[1]
+		choices = goodChoices[loadType]
+		goodSlots = []
+		for choice in choices:
+			for period in range(periodsPerDay):
+				l2 = []
+				for day in choice:
+					l2.append((day, period))
+				goodSlots.append(l2)
+		random.shuffle(goodSlots)
+		for slotGroup in goodSlots:
+			if(isSlotGroupAvailable(subject, slotGroup)):
+				for slot in slotGroup:
+					assignSubjectToSlot(subject, slot)
 				break
+				
 	return theSchedule
+
+def printSchedule(schedule):
+	for day in range(5):
+		for period in range(periodsPerDay):
+			slot = (day, period)
+			print str(schedule[slot]) + ' | ',
+		print
 
 setup()
 makeGraph()
-print schedule()
+sched = schedule()
+printSchedule(sched)
